6e38b195c2190bd8bcf098c5de936e6ac6113937
Caio J0rdan <caio10jordan@alu.ufc.br>
2024-05-10 15:26:11 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data.SqlTypes;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -15,11 +16,13 @@ hospedes.Add(p2);
 // Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
+//Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+ // Exibe a quantidade de hóspedes e o valor da diária
+ Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+ Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+
